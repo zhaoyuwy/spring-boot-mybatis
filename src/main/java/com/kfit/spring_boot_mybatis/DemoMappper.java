966701611df8c110116b75535bb6d6f317285766
zhaoyuwy <zhaoyuwy@163.com>
2017-02-19 04:01:53 +0800

    Spring Boot 集成Mybatis: --------------------------------------- 1、新建一个新的Maven Project; 2、需要在pom.xml文件添加相应的依赖，        比如：mysql驱动；        PageHelper分页插件，需要版本号； 3、编写启动类，大部分和之前的代码是一样的，需要的注意的是：     需要添加一个注解@MapperScan --指定MyBatis持久类的位置； 4、编写一个测试的实体类Demo; 5、编写一个DemoMapper,使用@Select和@Save进行数据库操作；    使用@Options配置返回的主键信息； 6、编写DemoService; 7、编写DemoController; 8、添加分页配置信息，需要添加一个MyBatisConfiguration; 9、使用PageHelper.startPage(pageNum,pageSize)进行分页；

--- src/main/java/com/kfit/spring_boot_mybatis/DemoMappper.java
+++ src/main/java/com/kfit/spring_boot_mybatis/DemoMappper.java
@@ -7,24 +7,24 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 public interface DemoMappper {
-	
-	//#{name}:参数占位符
-	@Select("select *from Demo where name=#{name}")
-	public List<Demo> likeName(String name);
-	
-	
-	@Select("select *from Demo where id = #{id}")
-	public Demo getById(long id);
-	
-	@Select("select name from Demo where id = #{id}")
-	public String getNameById(long id);
 
-	
-	/**
-	 * 保存数据.
-	 */
-	@Insert("insert into Demo(name) values(#{name})")
-	@Options(useGeneratedKeys=true,keyProperty="id",keyColumn="id")
-	public void save(Demo demo);
-	
+    //#{name}:参数占位符
+    @Select("select *from Demo where name=#{name}")
+    public List<Demo> likeName(String name);
+
+
+    @Select("select *from Demo where id = #{id}")
+    public Demo getById(long id);
+
+    @Select("select name from Demo where id = #{id}")
+    public String getNameById(long id);
+
+
+    /**
+     * 保存数据.
+     */
+    @Insert("insert into Demo(name) values(#{name})")
+//    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    public void save(Demo demo);
+
 }
--- src/main/java/com/kfit/spring_boot_mybatis/DemoService.java
+++ src/main/java/com/kfit/spring_boot_mybatis/DemoService.java
@@ -9,16 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class DemoService {
 
-	@Autowired
-	private DemoMappper demoMappper;
-	
-	public List<Demo> likeName(String name){
-		return demoMappper.likeName(name);
-	}
-	
-	@Transactional//添加事务.
-	public void save(Demo demo){
-		demoMappper.save(demo);
-	}
-	
+    @Autowired
+    private DemoMappper demoMappper;
+
+    public List<Demo> likeName(String name) {
+        return demoMappper.likeName(name);
+    }
+
+    @Transactional//添加事务.
+    public void save(Demo demo) {
+        demoMappper.save(demo);
+    }
+
 }
